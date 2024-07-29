81da1c262d2b274d29322fea11f78cf2dbc23580
Shivani D A <147906148+Jaishivani@users.noreply.github.com>
2024-07-29 17:34:33 +0200

    Create compound interest.py

---
+++ 29.07.2024/compound interest.py
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float p,r,t, CI;
+    printf("Enter amount: ");
+    scanf("%f", &p);
+    printf("Enter time: ");
+    scanf("%f", &t);
+    printf("Enter rate: ");
+    scanf("%f", &r);
+    CI = p*(pow((1+r/100),t));
+    printf("Compound Interest = %f", CI);
+}
